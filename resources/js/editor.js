83833ba585eb638df6f54825ac5cc93550fe7d61
Rahman Ramsi <rahmanramsi19@gmail.com>
2022-06-18 11:32:13 +0800

    add drag-drop

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -10,95 +10,94 @@ import Table from "@editorjs/table";
 import RawTool from "@editorjs/raw";
 import Delimiter from "@editorjs/delimiter";
 import { StyleInlineTool } from "editorjs-style";
+import DragDrop from "editorjs-drag-drop";
 
 document.addEventListener("alpine:init", () => {
-    Alpine.data(
-        "editorjs",
-        ({ state, statePath, placeholder, readOnly, tools }) => ({
-            instance: null,
-            state: state,
-            tools: tools,
-            init() {
-                let enabledTools = {};
+  Alpine.data(
+    "editorjs",
+    ({ state, statePath, placeholder, readOnly, tools }) => ({
+      instance: null,
+      state: state,
+      tools: tools,
+      init() {
+        let enabledTools = {};
 
-                if (this.tools.includes("header")) enabledTools.header = Header;
-                if (this.tools.includes("image")) {
-                    enabledTools.image = {
-                        class: ImageTool,
-                        config: {
-                            uploader: {
-                                uploadByFile: (file) => {
-                                    return new Promise((resolve) => {
-                                        this.$wire.upload(
-                                            `componentFileAttachments.${statePath}`,
-                                            file,
-                                            (uploadedFilename) => {
-                                                this.$wire
-                                                    .getComponentFileAttachmentUrl(
-                                                        statePath
-                                                    )
-                                                    .then((url) => {
-                                                        if (!url) {
-                                                            return resolve({
-                                                                success: 0,
-                                                            });
-                                                        }
-                                                        return resolve({
-                                                            success: 1,
-                                                            file: {
-                                                                url: url,
-                                                            },
-                                                        });
-                                                    });
-                                            }
-                                        );
-                                    });
-                                },
+        if (this.tools.includes("header")) enabledTools.header = Header;
+        if (this.tools.includes("image")) {
+          enabledTools.image = {
+            class: ImageTool,
+            config: {
+              uploader: {
+                uploadByFile: (file) => {
+                  return new Promise((resolve) => {
+                    this.$wire.upload(
+                      `componentFileAttachments.${statePath}`,
+                      file,
+                      (uploadedFilename) => {
+                        this.$wire
+                          .getComponentFileAttachmentUrl(statePath)
+                          .then((url) => {
+                            if (!url) {
+                              return resolve({
+                                success: 0,
+                              });
+                            }
+                            return resolve({
+                              success: 1,
+                              file: {
+                                url: url,
+                              },
+                            });
+                          });
+                      }
+                    );
+                  });
+                },
 
-                                uploadByUrl: (url) => {
-                                    return this.$wire
-                                        .loadImageFromUrl(url)
-                                        .then((result) => {
-                                            return {
-                                                success: 1,
-                                                file: {
-                                                    url: result,
-                                                },
-                                            };
-                                        });
-                                },
-                            },
-                        },
+                uploadByUrl: (url) => {
+                  return this.$wire.loadImageFromUrl(url).then((result) => {
+                    return {
+                      success: 1,
+                      file: {
+                        url: result,
+                      },
                     };
-                }
-                if (this.tools.includes("delimiter"))
-                    enabledTools.delimiter = Delimiter;
-                if (this.tools.includes("list")) enabledTools.list = List;
-                if (this.tools.includes("underline"))
-                    enabledTools.underline = Underline;
-                if (this.tools.includes("quote")) enabledTools.quote = Quote;
-                if (this.tools.includes("table")) enabledTools.table = Table;
-                if (this.tools.includes("raw")) enabledTools.raw = RawTool;
-                if (this.tools.includes("code")) enabledTools.code = Code;
-                if (this.tools.includes("inline-code"))
-                    enabledTools.inlineCode = InlineCode;
-                if (this.tools.includes("style"))
-                    enabledTools.style = StyleInlineTool;
-                this.instance = new EditorJS({
-                    holder: this.$el,
-                    minHeight: 30,
-                    data: this.state,
-                    placeholder: placeholder,
-                    readOnly: readOnly,
-                    tools: enabledTools,
-
-                    onChange: () => {
-                        this.instance.save().then((outputData) => {
-                            this.state = outputData;
-                        });
-                    },
-                });
+                  });
+                },
+              },
             },
-        })
-    );
+          };
+        }
+        if (this.tools.includes("delimiter"))
+          enabledTools.delimiter = Delimiter;
+        if (this.tools.includes("list")) enabledTools.list = List;
+        if (this.tools.includes("underline"))
+          enabledTools.underline = Underline;
+        if (this.tools.includes("quote")) enabledTools.quote = Quote;
+        if (this.tools.includes("table")) enabledTools.table = Table;
+        if (this.tools.includes("raw")) enabledTools.raw = RawTool;
+        if (this.tools.includes("code")) enabledTools.code = Code;
+        if (this.tools.includes("inline-code"))
+          enabledTools.inlineCode = InlineCode;
+        if (this.tools.includes("style")) enabledTools.style = StyleInlineTool;
+        this.instance = new EditorJS({
+          holder: this.$el,
+          minHeight: 30,
+          data: this.state,
+          placeholder: placeholder,
+          readOnly: readOnly,
+          tools: enabledTools,
+
+          onChange: () => {
+            this.instance.save().then((outputData) => {
+              this.state = outputData;
+            });
+          },
+          onReady: () => {
+            new DragDrop(this.instance);
+          },
+        });
+      },
+    })
+  );
 });
